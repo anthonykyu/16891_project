059407038faf7a462f3f6549b633666862de3745
Shivani Sivakumar <ssivaku3@andrew.cmu.edu>
2023-03-30 16:40:40 -0400

    Commented out A*

--- 16891_project_ws/src/mamp_planning/include/mamp_planning/astar.hpp
+++ 16891_project_ws/src/mamp_planning/include/mamp_planning/astar.hpp
@@ -15,12 +15,12 @@ public:
     shared_ptr<Agent> agent_;
     std::vector<constraint> constraints_;
     vector<shared_ptr<Vertex>> Astarpath_;
-    // AStar();
-    AStar(shared_ptr<Vertex> start, shared_ptr<Vertex> goal, shared_ptr<Agent> agent, std::vector<constraint> constraints);
-
-    void GetPath();
-    double GetEucDistance(shared_ptr<Vertex> v1, shared_ptr<Vertex> v2);
-    void backtrack(vector<shared_ptr<Vertex>> PRMgraph);
+    AStar();
+    // AStar(shared_ptr<Vertex> start, shared_ptr<Vertex> goal, shared_ptr<Agent> agent, std::vector<constraint> constraints);
+// 
+    // void GetPath();
+    // double GetEucDistance(shared_ptr<Vertex> v1, shared_ptr<Vertex> v2);
+    // void backtrack(vector<shared_ptr<Vertex>> PRMgraph);
 
 private:
 
