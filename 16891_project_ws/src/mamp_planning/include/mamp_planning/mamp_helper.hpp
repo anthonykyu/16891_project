ba6f38a5fee39d8aacc46baa0717da27d01be045
Anthony Kyu <akyu@andrew.cmu.edu>
2023-03-30 17:13:32 -0400

    Added and defined structs for collisions and constraints

--- 16891_project_ws/src/mamp_planning/include/mamp_planning/mamp_helper.hpp
+++ 16891_project_ws/src/mamp_planning/include/mamp_planning/mamp_helper.hpp
@@ -6,10 +6,18 @@
 // Question: How are we going to store the constraints?
 struct constraint
 {
-    int agent_id;
-    std::vector<double> joint_pos;
-    int time_step;
+    unsigned int int agent_id;
+    std::shared_ptr<Edge> joint_pos_edge;
+    double time_step;
+};
 
+struct collision
+{
+  unsigned int agent_id1;
+  unsigned int agent_id2;
+  std::shared_ptr<Edge> location1;
+  std::shared_ptr<Edge> location2;
+  double timestep;
 };
 
 // helper function file for detect_collisions function
