68a33159d0f2deef2c2f1c1b49251e0714774d9b
Shivani Sivakumar <ssivaku3@andrew.cmu.edu>
2023-04-13 11:33:52 -0400

    Added a few functions to dstarlite.hpp

--- 16891_project_ws/src/mamp_planning/include/mamp_planning/dstar_lite.hpp
+++ 16891_project_ws/src/mamp_planning/include/mamp_planning/dstar_lite.hpp
@@ -9,8 +9,23 @@
 class DStarLite
 {
 public:
-    DStarLite();
+    std::shared_ptr<Vertex> start_;
+    std::shared_ptr<Vertex> goal_;
+    std::shared_ptr<Agent> constraints_;
+    std::vector<std::shared_ptr<Vertex>> Dstarpath_;
+    DStarLite(double timestep);
+
+    bool computePRMPath(std::shared_ptr<Vertex> start, std::shared_ptr<Vertex> goal,
+                        std::pair<std::unordered_map<std::shared_ptr<Vertex>, std::vector<Constraint>>, std::unordered_map<std::shared_ptr<Edge>, std::vector<Constraint>>> &constraints,
+                        double max_constraint_time = 0);
+    std::vector<std::shared_ptr<Vertex>> getPRMPath(std::shared_ptr<Vertex> start, std::shared_ptr<Vertex> goal);
 
 private:
+    // TODO: need a function to calculate the key
+
+    double computeHeuristics(std::shared_ptr<Vertex> goal);
+    bool isValid(std::shared_ptr<Vertex> vertex, std::shared_ptr<Edge> edge);
+    bool isConstrained(std::shared_ptr<Vertex> vertex, std::shared_ptr<Edge> edge, double current_time,
+                        std::pair<std::unordered_map<std::shared_ptr<Vertex>, std::vector<Constraint>>, std::unordered_map<std::shared_ptr<Edge>, std::vector<Constraint>>> &constraints);
 
 };
